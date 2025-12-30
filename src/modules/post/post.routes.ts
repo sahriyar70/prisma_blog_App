7a2fbfaac72ef26fb206d67ab2d0e16ec555d41e
Al Sahriyar <alsahriyar229@gmail.com>
2025-12-30 17:20:43 +0600

    prisma blog app

--- src/modules/post/post.routes.ts
+++ src/modules/post/post.routes.ts
@@ -1,13 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { PostController } from "./post.controller";
+import { auth as betterauth } from "../../lib/auth";
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.post('/', PostController.createPost);    
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+
+    const session = await betterauth.api.getSession({
+      headers: req.headers as any,   // ✔️ এইটা লাগবে
+    });
+
+    console.log("SESSION:", session);
+
     
-    
+  };
+};
 
-
-
+router.post("/", auth("USER", "ADMIN"), PostController.createPost);
 
 export const postRoutes = router;
